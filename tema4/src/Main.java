799dd1be798c204bc4fad424d92e11fe0576e978
Oana Balint <oanab95@gmail.com>
2016-04-06 09:32:36 +0300

    tema 4

---
+++ tema4/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+                //Se da un numar. Sa se afiseze ‘true' daca numarul apartine intervalului [9,24]. Altfel sa se afiseze ‘false'.
+                int n = 21;
+                if (n>=9 && n<=24) {
+                    System.out.println("true");}
+                else{
+                    System.out.println("false");
+                }
+            }
+        }
